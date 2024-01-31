0f33f9ebf7702b39715db8cd77e020bb37b5bc38
luiscuartin <cuartinl@gmail.com>
2024-01-31 12:55:12 +0100

    Update Converso de temperatura.py

--- Converso de temperatura.py
+++ Converso de temperatura.py
@@ -1,5 +1,7 @@
 def conversortemperatura (celsius):
   Fahrenheit = (celsius * 9/5) +32
   return (Fahrenheit)
+Temperatura_celsius = 56
 
-print(conversortemperatura(56))
+print(conversortemperatura(Temperatura_celsius))
+
